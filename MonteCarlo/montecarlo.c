7d35076f8c875976dc1352cc5c595c8f1a4d8204
DEGUCHI Tomoya <tdeguchi@toriwaki.nuie.local>
2020-06-16 21:12:10 +0900

    montecarlo

--- MonteCarlo/montecarlo.c
+++ MonteCarlo/montecarlo.c
@@ -3,31 +3,28 @@
 #include <stdlib.h>
 
 double monte_carlo(int loop){
-    srand(time(NULL));
-
     double x, y;
     int i, cntr;
-    
-    FILE *fp;
-    fp = fopen("result.csv", "w");
+    cntr = 0;
 
     for (i=0; i < loop; i++){
+        srand(i);
         x = (double)rand() / RAND_MAX;
         y = (double)rand() / RAND_MAX;
 
         if(x*x + y*y < 1){
             cntr++;
-            printf("%lf, %lf\n", x, y);
+            //printf("%d, %lf, %lf\n", cntr, x, y);
         }
     }
-
-    return 4 * cntr / loop;
+    printf("%d/ %d\n", cntr, loop);
+    return (double)4 * (double)cntr / (double)loop;
 }
 
 int main(void)
 {
     int loop;
-    loop = 1000;
+    loop = 100000;
     printf("%lf\n", monte_carlo(loop));
 
     return 0;
